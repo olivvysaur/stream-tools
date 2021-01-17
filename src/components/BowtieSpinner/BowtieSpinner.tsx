a07fa9f3bbbd73a992f6aebb35a8de10500de27a
Olivia Asch <olivia.c.asch@gmail.com>
2021-01-17 16:21:54 +0000

    Fix row gap again and don’t try to be fancy with refs

--- src/components/BowtieSpinner/BowtieSpinner.tsx
+++ src/components/BowtieSpinner/BowtieSpinner.tsx
@@ -1,40 +1,28 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef } from 'react';
 
 import styles from './BowtieSpinner.module.css';
 
 const BOWTIE_WIDTH = 64;
 const BOWTIE_HEIGHT = 64;
-const ROW_GAP = 68;
+const ROW_GAP = 67;
 const COLUMN_GAP = 128;
 
+const ROWS = Math.ceil(1080 / (BOWTIE_HEIGHT + ROW_GAP)) + 1;
+const COLUMNS = Math.ceil(1920 / (BOWTIE_WIDTH + COLUMN_GAP)) + 1;
+
 export const BowtieSpinner = () => {
-  const [rows, setRows] = useState(0);
-  const [columns, setColumns] = useState(0);
-
   const canvas = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (!canvas.current) {
-      return;
-    }
-
-    const { width, height } = canvas.current.getBoundingClientRect();
-    setRows(Math.ceil(height / (BOWTIE_HEIGHT + ROW_GAP)) + 1);
-    setColumns(Math.ceil(width / (BOWTIE_WIDTH + COLUMN_GAP)) + 1);
-  }, [canvas]);
-
-  console.log(rows, columns);
 
   return (
     <div className={styles.canvas} ref={canvas}>
-      {[...Array(rows)].map((_, row) => {
+      {[...Array(ROWS)].map((_, row) => {
         const isOffset = row % 2 === 1;
 
         return (
           <div
             className={`${styles.row} ${isOffset ? styles.rowOffset : ''}`}
             key={row}>
-            {[...Array(isOffset ? columns + 2 : columns)].map((_, column) => (
+            {[...Array(isOffset ? COLUMNS + 2 : COLUMNS)].map((_, column) => (
               <div className={styles.column} key={column}>
                 <Bowtie />
               </div>
