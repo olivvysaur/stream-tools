0abf4f059c4443a3c11a5544183ac9d403d463e5
Olivia Asch <olivia.c.asch@gmail.com>
2021-01-23 15:00:32 +0000

    Make bowties spin at random

--- src/components/BowtieSpinner/BowtieSpinner.tsx
+++ src/components/BowtieSpinner/BowtieSpinner.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 import styles from './BowtieSpinner.module.css';
 
@@ -7,11 +7,46 @@ const BOWTIE_HEIGHT = 64;
 const ROW_GAP = 67;
 const COLUMN_GAP = 128;
 
-const ROWS = Math.ceil(1080 / (BOWTIE_HEIGHT + ROW_GAP)) + 1;
+const ROWS = Math.ceil(1080 / (BOWTIE_HEIGHT + ROW_GAP));
 const COLUMNS = Math.ceil(1920 / (BOWTIE_WIDTH + COLUMN_GAP)) + 1;
 
 export const BowtieSpinner = () => {
   const canvas = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!canvas.current) {
+      return;
+    }
+
+    const bowtieCollection = canvas.current.getElementsByClassName(
+      styles.bowtie
+    );
+    const allBowties: HTMLImageElement[] = [];
+
+    for (let i = 0; i < bowtieCollection.length; i++) {
+      allBowties.push(bowtieCollection[i] as HTMLImageElement);
+    }
+
+    const bowties = allBowties.filter((bowtie) => {
+      const { top, left, right, bottom } = bowtie.getBoundingClientRect();
+      return top < 1080 && left < 1920 && right > 0 && bottom > 0;
+    });
+
+    setInterval(() => {
+      const chosenIndex = Math.floor(Math.random() * bowties.length);
+      const chosenBowtie = bowties[chosenIndex] as HTMLImageElement;
+
+      chosenBowtie.classList.add(styles.spinning);
+      if (Math.random() < 0.25) {
+        chosenBowtie.classList.add(styles.reverse);
+      }
+
+      setTimeout(() => {
+        chosenBowtie.classList.remove(styles.spinning);
+        chosenBowtie.classList.remove(styles.reverse);
+      }, 1500);
+    }, 2000);
+  }, [canvas]);
 
   return (
     <div className={styles.canvas} ref={canvas}>
